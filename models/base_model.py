84f0bdce2986faa0ee1c4621eb638848f9758a83
sixtusagbo <miracleagbosixtus@gmail.com>
2022-11-29 02:45:44 +0100

    [Feature] Add BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Module that contains the base model from which other classes inherit from
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """ Common attributes/methods for other classes """
+
+    def __init__(self, *args, **kwargs):
+        """ Initialize attributes """
+        self.name = "Temp"
+        self.my_number = 98
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ Printable string representation """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
+
+    def save(self):
+        """ Updates `updated_at` with current datetime """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ dictionary of __dict__ of the instance modified """
+        dict_ = {}
+
+        for key, value in self.__dict__.items():
+            if key == 'created_at' or key == 'updated_at':
+                dict_[key] = value.strftime('%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                dict_[key] = value
+        dict_['__class__'] = self.__class__.__name__
+
+        return dict_
